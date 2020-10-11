ef01761d05207f3c0dc86bf34656024d61be9f08
Kyle <ziemerjkyle@gmail.com>
2020-10-11 19:14:31 -0400

    nits

--- backend/routes/authBackend.js
+++ backend/routes/authBackend.js
@@ -9,12 +9,14 @@ var demo_password = "mongoDEMO"
 var cloud_url = "mongodb+srv://demoUser:" + demo_password + "@cluster0.gtdqw.mongodb.net/demoDB?retryWrites=true&w=majority"
 
 
+//db usage info (I follow this general paradigm for the queries in all the routes, the queries are pretty simple)
+// https://www.w3schools.com/nodejs/nodejs_mongodb_create_db.asp
 router.get("/", function(req, res) {
     MongoClient.connect(cloud_url, function(err, db) { 
         var this_db = db.db('demoDB');
         this_db.collection('user_teams').find({"username": req.query.username}).toArray(function(err, result) { 
             if (result.length != 0) { 
-
+                //I assume there should just be one entry
                 username = result[0].username;
                 pw = result[0].password;
                 console.log(pw);
@@ -35,6 +37,7 @@ router.get("/", function(req, res) {
 });
 
 //I tried and failed to get the following two routes done in one route
+//Followed the same pattern for these routes as for the auto-generated one above (though I filled that one out)
 router.get("/checkUsername", function(req, res) { 
     MongoClient.connect(cloud_url, function(err, db) { 
         var this_db = db.db('demoDB');
@@ -55,7 +58,6 @@ router.post("/", function(req, res) {
     MongoClient.connect(cloud_url, function(err, db) { 
         var this_db = db.db('demoDB');
         this_db.collection('user_teams').insertOne(req.body, function(err, result) { 
-            if (err) throw err;
             res.send('User registered');
         }); 
         db.close();
